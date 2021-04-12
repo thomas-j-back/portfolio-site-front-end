67a82ebf469667c2ea6afa176662949764f9ac09
Thomas Back <thomasback@IntelleiveTBMBP.attlocal.net>
2021-04-11 17:46:02 -0700

    removing create react app earlier build

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: "",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader",
+        options: {
+          limit: "10000",
+          // ?limit=10000&name=img/[name].[ext]
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
